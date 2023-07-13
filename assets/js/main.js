338f470954c8b5afd3fccf78cc35e14dbcb0e13a
Vishnu Prakash <65424078+v-ishnu@users.noreply.github.com>
2023-07-14 02:22:42 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -162,7 +162,7 @@
   /**
    * Porfolio isotope and filter
    */
-  window.addEventListener('load', () => {
+ /* window.addEventListener('load', () => {
     let portfolioContainer = select('.portfolio-container');
     if (portfolioContainer) {
       let portfolioIsotope = new Isotope(portfolioContainer, {
@@ -187,7 +187,28 @@
       }, true);
     }
 
-  });
+  });*/
+
+
+
+
+
+var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+
+
+
+
+
+  
 
   /**
    * Initiate portfolio lightbox 
