445f93e84b3e497e69841137c10130cb6faa8519
Vishnu Prakash <65424078+v-ishnu@users.noreply.github.com>
2023-07-16 02:13:38 +0530

    Update validate.js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -3,7 +3,28 @@
 * URL: https://bootstrapmade.com/php-email-form/
 * Author: BootstrapMade.com
 */
-(function () {
+
+
+
+<?php
+
+if (isset($_POST['submit'])) {
+$name = $_POST['name'];
+$subject = $_POST['subject'];
+$mailFrom = $_POST['mail'];
+$message = $_POST['message'];
+
+
+$mailTo = "example@gmail.com";
+$headers = "From: ".$mailFrom;
+$txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+mail($mailTo, $subject, $txt, $headers);
+header("Location: index.php?mailsend");
+}
+
+
+/*(function () {
   "use strict";
 
   let forms = document.querySelectorAll('.php-email-form');
@@ -82,4 +103,4 @@
     thisForm.querySelector('.error-message').classList.add('d-block');
   }
 
-})();
+})();*/
