23a01afd1c7c34166c802e720981b2e10de61db2
Vishnu Prakash <65424078+v-ishnu@users.noreply.github.com>
2023-07-16 02:20:02 +0530

    Update validate.js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -6,25 +6,9 @@
 
 
 
-<?php
-
-if (isset($_POST['submit'])) {
-$name = $_POST['name'];
-$subject = $_POST['subject'];
-$mailFrom = $_POST['mail'];
-$message = $_POST['message'];
 
 
-$mailTo = "vishnuprakash572@gmail.com";
-$headers = "From: ".$mailFrom;
-$txt = "You have received an e-mail from ".$name.".\n\n".$message;
-
-mail($mailTo, $subject, $txt, $headers);
-header("Location: index.php?mailsend");
-}
-
-
-/*(function () {
+(function () {
   "use strict";
 
   let forms = document.querySelectorAll('.php-email-form');
@@ -103,4 +87,4 @@ header("Location: index.php?mailsend");
     thisForm.querySelector('.error-message').classList.add('d-block');
   }
 
-})();*/
+})();
