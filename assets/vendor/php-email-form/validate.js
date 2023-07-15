dfb4f15ef1ab3873aa0f0b7f3c0d8f33805097c9
Vishnu Prakash <65424078+v-ishnu@users.noreply.github.com>
2023-07-16 02:16:08 +0530

    Update validate.js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -15,7 +15,7 @@ $mailFrom = $_POST['mail'];
 $message = $_POST['message'];
 
 
-$mailTo = "example@gmail.com";
+$mailTo = "vishnuprakash572@gmail.com";
 $headers = "From: ".$mailFrom;
 $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
